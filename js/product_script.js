8baad3697a918db5a3180f6f56249a8eacb504d3
dominion-30 <abiolaayobamidele@gmail.com>
2023-04-06 11:28:18 +0100

    Add file

--- js/product_script.js
+++ js/product_script.js
@@ -11,13 +11,14 @@ let value_of_param = urlSearch.searchParams.get("id");
 fetch(`https://fakestoreapi.com/products/${value_of_param}`)
 .then(request=>request.json())
 .then(json=>{
-    let item = `<div>
+    let item = `<div class="container">
     <h3>${json.title}</h3>
     <p><strong>Price:${json.price}</strong></p>
     <p><strong>Category:${json.category}</strong></p>
     <p><strong>Description:${json.description}</strong></p>
     <div><img src="${json.image}" alt="image of product"></div>
-    </div>`;
+    </div>
+    <div><a href="/">Proceed to Payment</a></div>`;
 
     document.getElementById("product").innerHTML = item;
 });
