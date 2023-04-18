6dacd8877c35a503a8362e09bc53812428ef02c7
dominion-30 <abiolaayobamidele@gmail.com>
2023-04-18 14:01:21 +0100

    Add changes

---
+++ js/login_script.js
@@ -0,0 +1,23 @@
+function submitUser(){
+
+    let user_name = document.getElementById("user_name").value;
+    let user_password = document.getElementById("user_password").value;
+    console.log(user_name, user_password);
+}
+// let user_value = document.getElementById("user_name").value;
+// console.log(user_value);
+
+
+
+
+
+// fetch('https://fakestoreapi.com/auth/login',{
+//             method:'POST',
+//             body:JSON.stringify({
+//                 username: "johnd",
+//                 password: "m38rmF$"
+//             })
+//         })
+//             .then(res=>res.json())
+//             .then(json=>console.log(json));
+
